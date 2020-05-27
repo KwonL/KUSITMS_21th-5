d67ca5606e28c3bea9a968e92fc1b4a2b1159ae4
KwonL <lkh116@snu.ac.kr>
2020-05-27 19:02:44 +0900

    ref | Change hardcoded list to map

--- screens/GalleryScreen/index.js
+++ screens/GalleryScreen/index.js
@@ -5,35 +5,38 @@ import { KakaoRegularText, KakaoBoldText } from '../../components/StyledText';
 import { SliderBox } from "react-native-image-slider-box";
 
 export default GalleryScreen = (props) => {
-  const data = {
-    user_id: {
-      id: 'kkyu_98',
+  const data = [
+    {
+      photo_container: {
+        month: 5,
+        day: 15,
+        total_calorie: 2705,
+      },
+      images: [
+        require('../../assets/images/examples/kkhokkho.png'),
+        require('../../assets/images/examples/myfood1.png'),
+        require('../../assets/images/examples/515lunch.png')
+      ],
+      components: [
+        '매추리알', '양념치킨', '라면사리', '떡갈비', '김치찌개', '공기밥', '보쌈', '김치',
+      ],
     },
-    photo_container1: {
-      month: 5,
-      day: 15,
-      total_calorie: 2705,
-    },
-    images1: [
-      require('../../assets/images/examples/kkhokkho.png'),
-      require('../../assets/images/examples/myfood1.png'),
-      require('../../assets/images/examples/515lunch.png')
-    ],
-    componests1: [
-      '매추리알', '양념치킨', '라면사리', '떡갈비', '김치찌개', '공기밥', '보쌈', '김치',
-    ],
+    {
+      photo_container: {
+        month: 5,
+        day: 14,
+        total_calorie: 1900,
+      },
+      images: [
+        require('../../assets/images/examples/myfood2.png'),
+        require('../../assets/images/examples/maramara.png'),
+      ],
+      components: [
+        '하이루', '방가방가'
+      ]
+    }
+  ]
 
-    photo_container2: {
-      month: 5,
-      day: 14,
-      total_calorie: 1900,
-    },
-
-    images2: [
-      require('../../assets/images/examples/myfood2.png'),
-      require('../../assets/images/examples/maramara.png'),
-    ]
-  }
   return (
     <View style={styles.container}>
       <View
@@ -44,39 +47,27 @@ export default GalleryScreen = (props) => {
         <KakaoBoldText style={styles.galleryText}>갤러리</KakaoBoldText>
       </View>
       <ScrollView style={styles.container}>
-
-        <View style={[styles.photoContainer, { height: 550 }]}>
-          <KakaoRegularText style={styles.date}>{data.photo_container1.month}월{data.photo_container1.day}일</KakaoRegularText>
-          <KakaoRegularText style={styles.calorie}>{data.photo_container1.total_calorie} kcal</KakaoRegularText>
-          <SliderBox images={data.images1}
-            sliderBoxHeight={400}
-            marginTop={10}
-            dotColor="#FFEE58"
-            inactiveDotColor="#B5BACE"
-          />
-
-          <View style={styles.hashtag_container}>
-            <KakaoRegularText style={styles.hashtag}> #{data.componests1[0]}
-            </KakaoRegularText>
-            <KakaoRegularText style={styles.hashtag}> #{data.componests1[1]}
-            </KakaoRegularText>
-            <KakaoRegularText style={styles.hashtag}> #{data.componests1[2]}
-            </KakaoRegularText>
-          </View>
-
-        </View>
-
-        <View style={[styles.photoContainer, { height: 550 }]}>
-          <KakaoRegularText style={styles.date}>{data.photo_container2.month}월{data.photo_container2.day}일</KakaoRegularText>
-          <KakaoRegularText style={styles.calorie}>{data.photo_container2.total_calorie} kcal</KakaoRegularText>
-          <SliderBox images={data.images2}
-            sliderBoxHeight={400}
-            marginTop={10}
-            dotColor="#FFEE58"
-            inactiveDotColor="#B5BACE"
-          />
-
-        </View>
+        {data.map(item => {
+          return (
+            <View style={[styles.photoContainer, { height: 550 }]}>
+              <KakaoRegularText style={styles.date}>{item.photo_container.month}월{item.photo_container.day}일</KakaoRegularText>
+              <KakaoRegularText style={styles.calorie}>{item.photo_container.total_calorie} kcal</KakaoRegularText>
+              <SliderBox images={item.images}
+                sliderBoxHeight={400}
+                marginTop={10}
+                dotColor="#FFEE58"
+                inactiveDotColor="#B5BACE"
+              />
+              <View style={styles.hashtag_container}>
+                {item.components.map(hash => {
+                  return (
+                    <KakaoRegularText style={styles.hashtag}>#{hash}</KakaoRegularText>
+                  )
+                })}
+              </View>
+            </View>
+          )
+        })}
       </ScrollView>
     </View>
   );
--- screens/GalleryScreen/style.js
+++ screens/GalleryScreen/style.js
@@ -5,47 +5,38 @@ export default styles = StyleSheet.create({
         backgroundColor: '#F1F0F2',
         flex: 1
     },
-
     galleryText: {
-        fontSize : 26,
-        marginTop : 18,
-        alignSelf : 'center',
+        fontSize: 26,
+        marginTop: 18,
+        alignSelf: 'center',
         marginRight: 'auto',
         marginLeft: 40,
         marginBottom: 25,
     },
-
-    photoContainer :{
+    photoContainer: {
         marginTop: 1,
         backgroundColor: '#FFFFFF',
     },
-
-    date :{
-        fontSize : 17,
-        marginTop : 26,
-        marginLeft : 28,
+    date: {
+        fontSize: 17,
+        marginTop: 26,
+        marginLeft: 28,
     },
-    
-    calorie :{
-        fontSize : 22,
+    calorie: {
+        fontSize: 22,
         color: '#D47FA6',
-        marginLeft : 280,
-        marginTop : -22,
+        marginLeft: 280,
+        marginTop: -22,
     },
-
-    hashtag_container :{
-        flex : 1,
-        flexDirection : 'row',
-        marginTop : 'auto',
-
+    hashtag_container:{
+        flex: 1,
+        flexDirection: 'row',
+        marginTop: 'auto',
     },
-
-    hashtag :{
-        fontSize :17,
-        color : "#334856",
-        marginTop : 20,
-        marginLeft : 10,
+    hashtag: {
+        fontSize: 17,
+        color: "#334856",
+        marginTop: 20,
+        marginLeft: 10,
     }
-
-   
 });
